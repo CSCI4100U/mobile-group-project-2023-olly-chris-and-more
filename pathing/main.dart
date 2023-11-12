275a7ec4408fffd6230b6e50f9babb02b76d1561
Chris Chen <91335338+ChrisKHChen@users.noreply.github.com>
2023-11-12 18:24:21 -0500

    Refactor

--- pathing/main.dart
+++ pathing/main.dart
@@ -29,10 +29,10 @@ class _MovingDotState extends State<MovingDot> with SingleTickerProviderStateMix
   final double downMove100 = 100.0;
   final double rightMove200 = 200.0;
   final double downMove150 = 150.0;
-  final double leftMove100 = 100.0; 
+  final double leftMove100 = 100.0;
   final double downMove200 = 200.0;
   bool isAnimating = false;
-  bool isWave1Started = false;
+  bool wave1Start = false;
 
   @override
   void initState() {
@@ -42,61 +42,48 @@ class _MovingDotState extends State<MovingDot> with SingleTickerProviderStateMix
   void startAnimation() {
     if (!isAnimating) {
       isAnimating = true;
-
-      final Duration duration = Duration(seconds: 20); 
-
+      final Duration duration = Duration(seconds: 20);
       final controller = AnimationController(
         duration: duration,
         vsync: this,
       );
-
       controller.addListener(() {
         final progress = controller.value;
 
         if (progress < 0.1) {
-          // Move right 100 units
           dotPositionX = progress * 10 * rightMove100;
-          dotPositionY = 100.0; // Adjusted initial Y position
+          dotPositionY = 100.0;
         } else if (progress < 0.2) {
-          // Move down 100 units
           dotPositionX = rightMove100;
           dotPositionY = 100.0 + (progress - 0.1) * 10 * downMove100;
         } else if (progress < 0.3) {
-          // Move right 200 units
           dotPositionX = rightMove100 + (progress - 0.2) * 10 * rightMove200;
           dotPositionY = 100.0 + downMove100;
         } else if (progress < 0.4) {
-          // Move down 150 units
           dotPositionX = rightMove100 + rightMove200;
           dotPositionY = 100.0 + downMove100 + (progress - 0.3) * 10 * downMove150;
         } else if (progress < 0.5) {
-          // Move left 100 units
           dotPositionX = rightMove100 + rightMove200 - (progress - 0.4) * 10 * leftMove100;
           dotPositionY = 100.0 + downMove100 + downMove150;
         } else if (progress < 0.6) {
-          // Move down 100 units
           dotPositionX = rightMove100 + rightMove200 - leftMove100;
           dotPositionY = 100.0 + downMove100 + downMove150 + (progress - 0.5) * 10 * downMove100;
         } else if (progress < 1.0) {
-          // Move right 200 units
           dotPositionX = rightMove100 + rightMove200 - leftMove100 + (progress - 0.6) * 10 * rightMove200;
           dotPositionY = 100.0 + downMove100 + downMove150 + downMove100;
         }
-
         setState(() {});
-
         if (progress == 1.0) {
           isAnimating = false;
+          wave1Start = false;
         }
       });
-
       controller.forward();
+      wave1Start = true;
 
-      //Wave message
-      isWave1Started = true;
-      Timer(Duration(seconds: 2), () {
+      Timer(Duration(seconds: 4), () {
         setState(() {
-          isWave1Started = false;
+          wave1Start = false;
         });
       });
     }
@@ -127,15 +114,16 @@ class _MovingDotState extends State<MovingDot> with SingleTickerProviderStateMix
               ),
             ),
           ),
-          Positioned(
-            top: 0,
-            right: 0,
-            child: ElevatedButton(
-              onPressed: startAnimation,
-              child: Text('Wave Start'),
+          if (!wave1Start)
+            Positioned(
+              top: 0,
+              right: 0,
+              child: ElevatedButton(
+                onPressed: startAnimation,
+                child: Text('Wave Start'),
+              ),
             ),
-          ),
-          if (isWave1Started)
+          if (wave1Start)
             Center(
               child: Text(
                 'Wave 1 has Started',
