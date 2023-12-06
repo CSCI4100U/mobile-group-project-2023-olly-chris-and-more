dec6c36ae13dd12630de7d63a19ea06f23d84181
gituser1034 <90145189+gituser1034@users.noreply.github.com>
2023-12-06 00:12:12 -0500

    Delete final_barnyard_defenders directory

--- final_barnyard_defenders/android/app/src/main/kotlin/com/example/final_barnyard_defenders/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.final_barnyard_defenders
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- final_barnyard_defenders/lib/omed_work/firebase.dart
+++
@@ -1,8 +0,0 @@
-import 'sqlite.dart';
-
-// What is this
-final databaseReference = FirebaseDatabase.instance.reference();
-
-void uploadGameStats(GameStats stats) {
-  databaseReference.child("stats").push().set(stats.toMap());
-}
