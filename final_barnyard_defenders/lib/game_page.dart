250aae7fc5d85086dc9e354db90e27dab823e693
Chris Chen <91335338+ChrisKHChen@users.noreply.github.com>
2023-12-07 04:39:22 -0500

    wave screen outline

--- final_barnyard_defenders/lib/game_page.dart
+++ final_barnyard_defenders/lib/game_page.dart
@@ -3,12 +3,34 @@ import 'package:flutter/material.dart';
 import 'barnyard_defenders_game.dart';
 
 class GamePage extends StatelessWidget {
-  const GamePage({super.key});
+  const GamePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    // Create an instance of BarnyardDefendersGame
     BarnyardDefendersGame game = BarnyardDefendersGame();
-    return GameWidget(game: game);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Barnyard Defenders"),
+      ),
+      body: Stack(
+        children: [
+          GameWidget(game: game),
+          // Wave Start button
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                // Call the startWave method when the button is pressed
+                game.startWave();
+              },
+              child: Text("Wave Start"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
-
